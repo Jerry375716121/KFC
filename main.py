e587b4854ef63b27d48cdc206ca6408be36c0c3d
Jerry <Jerry375716121@gmail.com>
2023-02-15 08:55:59 +0800

    V 2.8

--- main.py
+++ main.py
@@ -28,10 +28,15 @@ class Preparing:
         else:
             return False
 
-    def insert(self, value):
+    def insert(self, value):#记得修bug
         new_node = Order(value)
         new_node.next = self.head
         self.head = new_node
+
+    def mid_insert(self, value): #中间插入，实现插队效果
+        new_node = Order(value)
+
+        return
 
     def delete(self, item):
         now = self.head
@@ -54,13 +59,14 @@ class Preparing:
 
 Preparing_list = Preparing()
 Ready_list = []
-menu = [['Hamburger', 20], ['Milk Tea', 25], ['Cola', 8], ['Fresh-Made Salad', 7], ['Vanilla Frosty', 10], ['Large Fries', 15], ['Middle Fries', 12]]
+menu = [['Hamburger', 20], ['Milk Tea', 25], ['Cola', 8], ['Fresh-Made Salad',
+                                                           7], ['Vanilla Frosty', 10], ['Large Fries', 15], ['Middle Fries', 12]]
 
 
-def set_menu(): #生成menu
-    print('✿ MENU'.rjust(25),'✿')
+def set_menu():  # 生成menu
+    print('✿ MENU'.rjust(25), '✿')
     for i in range(len(menu)):
-            print(i+1,'',menu[i][0].ljust(20),'  $'.rjust(20), menu[i][1])
+        print(i+1, '', menu[i][0].ljust(20), '  $'.rjust(20), menu[i][1])
     return menu
 
 
@@ -73,7 +79,7 @@ def GetOrderID():  # 按照顺序生成取餐码
     return
 
 
-def get_total_price(food_arr): #计算总价
+def get_total_price(food_arr):  # 计算总价
     total = 0
     found = False
     for num in food_arr:
@@ -82,27 +88,28 @@ def get_total_price(food_arr): #计算总价
     return total
 
 
-def Cut_in(orders,ID): #插队
-    flag=input('Do you want to pay 10% more to cut into the queue? \nY for yes N for no\n')
-    if flag=='Y':
+def Cut_in(orders, ID):  # 插队
+    flag = input(
+        'Do you want to pay 10% more to cut into the queue? \nY for yes N for no\n')
+    if flag == 'Y':
         print('You have been moved forward\nTotal price is:')
 
-
-
         return get_total_price(orders)*1.1
-    elif flag=='N':
+    elif flag == 'N':
         print('OK, total price is:')
         Preparing_list.insert(ID)
         return get_total_price(orders)
 
+
 def Ordering():  # 用户点餐--
     set_menu(menu)
-    ID=GetOrderID()
+    ID = GetOrderID()
     sum_price = 0
     order_food_arr = []
     want = input('Do you want to order something? (yes or no):')
     if want == 'yes':
-        order_food=(input('please choose your meal(by entering the serial number):'))
+        order_food = (
+            input('please choose your meal(by entering the serial number):'))
         for content in order_food:
             if content.isnumeric():
                 if int(content) <= 7:
@@ -112,7 +119,7 @@ def Ordering():  # 用户点餐--
     else:
         print('Thank you for using the system')
     print(order_food_arr)
-    print('Total price is:',Cut_in(order_food_arr,ID))
+    print('Total price is:', Cut_in(order_food_arr, ID))
 
 
 def Transfer_to_Ready(order_id):  # 制作完成，通知取餐
